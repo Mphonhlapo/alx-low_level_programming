a132b9702e47809a7e74642a22add21f61ede914
Mpho Mishack <mpho686@gmail.com>
2023-07-15 03:01:02 -0700

    corrections after learning betty

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ *main-A program that prints using printf funtion instead of puts
+ *Return: 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+retun(0);
+}
