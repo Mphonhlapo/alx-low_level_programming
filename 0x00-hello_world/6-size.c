50ffdac53be79811aa345292a4f0dc6eb34c1ca3
Mpho Mishack <mpho686@gmail.com>
2023-07-15 17:11:25 -0700

    checking line continuation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 /*
- * main -A C program that prints the size of various types on the computer it is compiled and run on.
+ * main - Entry point of aprogram
+ *
+ * A C program that prints the size of various types,
+ * on the computer it is compiled and run on.
+ *
  * return: 0
  */
 int main(void)
@@ -8,7 +12,8 @@ int main(void)
 printf("Size of a char: %1d byte(s)\n", (unsigned long) sizeof(char));
 printf("Size of an int: %1d byte(s)\n", (unsigned long) sizeof(int));
 printf("Size of a long int: %1d byte(s)\n", (unsigned long) sizeof(long int));
-printf("Size of a long long int: %1d byte(s)\n", (unsigned long) sizeof(long long int));
+printf("Size of a long long int: %1d byte(s)\n"\
+		, (unsigned long) sizeof(long long int));
 printf("Size of a float: %1d byte(s)\n", (unsigned long) sizeof(float));
 return (0);
 }
