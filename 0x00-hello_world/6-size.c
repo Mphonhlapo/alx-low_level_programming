a4c176546fa6aadc438a606e323864f70403dc9e
Mpho Mishack <mpho686@gmail.com>
2023-07-15 17:17:19 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /*
- * main - Entry point of aprogram
+ * Main - Entry point of aprogram
  *
  * A C program that prints the size of various types,
  * on the computer it is compiled and run on.
@@ -12,8 +12,7 @@ int main(void)
 printf("Size of a char: %1d byte(s)\n", (unsigned long) sizeof(char));
 printf("Size of an int: %1d byte(s)\n", (unsigned long) sizeof(int));
 printf("Size of a long int: %1d byte(s)\n", (unsigned long) sizeof(long int));
-printf("Size of a long long int: %1d byte(s)\n"\
-		, (unsigned long) sizeof(long long int));
+printf("Size of a long long int: %1d byte(s)\n", (unsigned long) sizeof(long long int));
 printf("Size of a float: %1d byte(s)\n", (unsigned long) sizeof(float));
 return (0);
 }
