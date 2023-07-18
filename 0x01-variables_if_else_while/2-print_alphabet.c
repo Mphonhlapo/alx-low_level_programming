738b883c5f509b7e90ea4057a6c85be653773177
Mpho Mishack <mpho686@gmail.com>
2023-07-17 20:44:05 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'Printing lowcase characters'
+ * Return: always 0
+ */
+int main(void)
+{
+	int n = 97
+
+	while (n < 123)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n')
+	return (0);
+}
