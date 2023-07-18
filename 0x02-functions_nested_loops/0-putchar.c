69d7ae6c29e9d4678d629cc1d9e98eb4f1c1507c
Mpho Mishack <mpho686@gmail.com>
2023-07-18 13:47:59 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ * Description - A program that prints text followed by a line.
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
