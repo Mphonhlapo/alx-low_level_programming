447c8bc5c4e898edcdd5b62323a2076bf0cdfe72
Mpho Mishack <mpho686@gmail.com>
2023-09-08 07:31:14 -0700

    corrections of 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,48 +1,34 @@
 #include "main.h"
-#include <fcntl.h>
-#include <unistd.h>
 
 /**
- * append_text_to_file - Appends text to the end of a file.
- * @filename: A pointer to the name of the file.
- * @text_content: The string to add to the end of the file.
- *
- * Return: On failure or if filename is NULL, return -1.
- *         If file does not exist or user lacks write permissions, return -1.
- *         Otherwise, return 1 upon successful append.
- */
+* append_text_to_file - Appends text to the end of a file.
+* @filename: A pointer to the name of the file.
+* @text_content: The string to add to the end of the file.
+*
+* Return: If the function fails or filename is NULL - returns -1.
+*         If file does not exist or user lacks write permissions - returns -1.
+*         Otherwise - returns 1 on success.
+*/
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int o, w, len = 0;
-	int result = 1; /* Default return value is success */
+	int file_descriptor, write_result, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len]; len++)
-			; /* Calculate the length using a for loop */
+		for (len = 0; text_content[len];)
+			len++;
 	}
 
-	o = open(filename, O_WRONLY | O_APPEND);
-	if (o == -1)
-		return (-1); /* open failed */
+	file_descriptor = open(filename, O_WRONLY | O_APPEND);
+	write_result = write(file_descriptor, text_content, len);
 
-	if (text_content != NULL)
-	{
-		for (int i = 0; i < len; i++)
-		{
-			w = write(o, &text_content[i], 1);
-			if (w == -1)
-			{
-				result = -1; /* write failed */
-				break;
-			}
-		}
-	}
+	if (file_descriptor == -1 || write_result == -1)
+		return (-1);
 
-	close(o);
+	close(file_descriptor);
 
-	return (result);
+	return (1);
 }
