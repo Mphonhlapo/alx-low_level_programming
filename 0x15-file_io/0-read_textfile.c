7553ba605ef48d80345cead29eac9a9443ff2144
Mpho <mpho686@gmail.com>
2023-10-02 17:44:16 -0700

    1st file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,54 +1,26 @@
 #include "main.h"
 
 /**
- * read_textfile - Read a text file and print its content to STDOUT.
- * @filename: Text file to be read.
- * @letters: Number of letters to be read.
+ * read_textfile - Read from a text file and print to STDOUT.
+ * @filename: Name of the file to read from.
+ * @letters: Number of letters to read and print.
  *
- * Return: The actual number of bytes read and printed, or -1 on failure.
+ * Return: The actual number of bytes printed, or 0 on failure.
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	if (filename == NULL)
-	{
-		fprintf(stderr, "Filename is NULL.\n");
-		return -1; /*indicates failure */
-	}
+	char *buf;
+	ssize_t fd, bytes_written, total_bytes;
 
-	int fd = open(filename, O_RDONLY);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-	{
-		perror("Unable to open file");
-		return -1; /* indicates failure */
-	}
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	total_bytes = read(fd, buf, letters);
+	bytes_written = write(STDOUT_FILENO, buf, total_bytes);
 
-	char *buf = (char *)malloc(sizeof(char) * letters);
-	if (buf == NULL)
-	{
-		perror("Memory allocation failed");
-		close(fd);
-		return -1; /*indicates failure */
-	}
-
-	ssize_t bytes_read = read(fd, buf, letters);
-	if (bytes_read == -1)
-	{
-		perror("Read failed");
-		free(buf);
-		close(fd);
-		return -1; /* Indicates failure */
-	}
-
-	ssize_t bytes_written = write(STDOUT_FILENO, buf, bytes_read);
-	if (bytes_written == -1)
-	{
-		perror("Write failed");
-		free(buf);
-		close(fd);
-		return -1; /*indicates failure */
-	}
-i
 	free(buf);
 	close(fd);
-	return bytes_written;
+	return (bytes_written);
 }
