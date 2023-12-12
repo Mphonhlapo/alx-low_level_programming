724c17dfa319c4865de68efffe37faeb6bc7151e
Mpho Nhlapo <mpho686@gmail.com>
2023-12-12 09:44:34 -0800

    adding more static files

---
+++ 0x18-dynamic_libraries/functions/subfunctions/_abs.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+*_abs - empty function
+*@n: string
+*Return: nothing
+*/
+
+int _abs(int n)
+{
+}
---
+++ 0x18-dynamic_libraries/functions/subfunctions/isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *_isalpha - an empty function
+ *@c: string
+ *Return: nothing
+ */
+
+
+int _isalpha(int c)
+{
+}
